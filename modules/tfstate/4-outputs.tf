cabf3d399affb29ef1fe3621fa357e98a32af794
Timotej979 <timotej.pet@gmail.com>
2024-10-13 11:07:27 +0200

    Fix outputs for tfstate, test out the deployment

--- modules/tfstate/4-outputs.tf
+++ modules/tfstate/4-outputs.tf
@@ -1,14 +1,14 @@
 output "bucket_name" {
   description = "The name of the S3 bucket"
-  value = module.aws_s3_bucket.s3_bucket_name
+  value = aws_s3_bucket.terraform_state.bucket
 }
 
 output "bucket_arn" {
   description = "The ARN of the S3 bucket"
-  value = module.aws_s3_bucket.s3_bucket_arn
+  value = aws_s3_bucket.terraform_state.arn
 }
 
 output "dynamodb_table_name" {
   description = "Name of the DynamoDB table"
-  value       =  aws_dynamodb_table.this.name
+  value       =  aws_dynamodb_table.terraform_state_lock.name
 }
